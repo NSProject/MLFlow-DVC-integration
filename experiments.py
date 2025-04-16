a18d50494290df2314a13526d59544cfae071b6a
ns loni <nsloni@gmail.com>
2025-04-16 19:40:56 +0530

    add

---
+++ experiments.py
@@ -0,0 +1,29 @@
+import mlflow
+
+def calculator(a, b, operation=None):
+    if operation == "add":
+        return a + b
+    elif operation == "sub":
+        return a - b
+    elif operation == "mul":
+        return a * b
+    elif operation == "div":
+        return a / b
+    else:
+        raise ValueError("Invalid operation specified.")
+
+if __name__ == "__main__":
+    a, b, opt = 1084, 3140, "add"
+    
+    with mlflow.start_run():
+        result = calculator(a, b, opt)
+
+        # Log input parameters and the operation type
+        mlflow.log_param("a", a)
+        mlflow.log_param("b", b)
+        mlflow.log_param("operation", opt)
+
+        # Log the result as a metric (since it's numerical)
+        mlflow.log_metric("result", result)
+
+        print(f"My result is {result}")
